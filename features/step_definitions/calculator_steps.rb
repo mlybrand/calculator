cde5594a726e7534caeaf3ad6ada0eb030ef2146
Mark Lybrand <mlybrand@gmail.com>
2017-01-23 10:46:11 -0800

    implemented more test cases and workable solution

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -9,5 +9,5 @@ When(/^the calculator is run$/) do
 end
 
 Then(/^the output should be "([^"]*)"$/) do |expected_output|
-    @output.should == expected_output
+    expect(@output).to eq(expected_output)
 end
